8da25f5881efe86f6a4c4a080e0f189dad1575fc
BASIC <85481790+BASIC-scripter@users.noreply.github.com>
2021-07-09 11:39:13 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+This script and retexture was created by BASIC. 
+Showcase: https://youtu.be/MbB6afqJ2YA
+
---
+++ grenadelauncher_lesslethal/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+game "gta5"
+
+name "grenadelauncher_lesslethal"
+description "Converted Grenade Launcher to Non-Letal Ammunition Launcher"
+author ": BASIC"
+version "1.0"
+
+files {
+  'weapons.meta'
+}
+
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons.meta'
